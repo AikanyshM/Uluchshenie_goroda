c1ba0ffdcd18d96aae71b22613665659fcfca123
Aikanysh Matyeva <aikanyshmatyeva@MacBook-Air-Elmira.local>
2022-11-26 23:29:12 +0600

    Started a new project gorod

---
+++ main_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+
+
+User = get_user_model()
+
+class Citizen(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name = _("Гражданин"))
