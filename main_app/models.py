0fd5cc2e6c304d3ad624e55990774a360b6a6220
Aikanysh Matyeva <aikanyshmatyeva@MacBook-Air-Elmira.local>
2022-11-26 23:58:39 +0600

    Created urls

--- main_app/models.py
+++ main_app/models.py
@@ -4,3 +4,9 @@ from account.models import User
 class Category(models.Model):
     title = models.CharField(max_length=100, verbose_name = "Название категории")
     text = models.TextField(verbose_name = "Описание категории")
+
+class News(models.Model):
+    name = models.CharField(max_length=255, verbose_name= 'Краткое название')
+    description = models.TextField(verbose_name='Описание новости')
+    main_photo = models.ImageField(verbose_name=('Фото новости'), upload_to='news_main_image')
+    date = models.DateField(auto_now_add=True, verbose_name= 'Дата новости')
