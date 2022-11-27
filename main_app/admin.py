414fb0762a1cce3b0b26499fb3112aab5a0d98f4
Aikanysh Matyeva <aikanyshmatyeva@MacBook-Air-Elmira.local>
2022-11-27 13:02:40 +0600

    Updated admin panel

--- main_app/admin.py
+++ main_app/admin.py
@@ -10,3 +10,8 @@ class NewsAdmin(admin.ModelAdmin):
 
 class ApplicationAdmin(admin.ModelAdmin):
     list_display = ('title', 'text', 'application_photo', 'date', 'location', 'user', 'category')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(News, NewsAdmin)
+admin.site.register(Application, ApplicationAdmin)
