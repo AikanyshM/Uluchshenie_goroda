30c56f01f65e8fb2c535637fe4fa4c3d7b01c3e3
Aikanysh Matyeva <aikanyshmatyeva@MacBook-Air-Elmira.local>
2022-11-27 12:41:15 +0600

    Updated CORS

--- gorod/settings.py
+++ gorod/settings.py
@@ -143,3 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'account.User'
 LOGIN_REDIRECT_URL = '/auth/profile/'
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
